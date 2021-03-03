47db4b4ccb7a9e6028e5c4140c9fd0e0f8001581
Austin Eng <enga@chromium.org>
2021-03-03 13:13:41 -0800

    Revert "wip"
    This reverts commit 2b5a34a0c5725609cb5fb6cc59ac0513515c5810.

--- src/common/tools/crawl.ts
+++ src/common/tools/crawl.ts
@@ -14,14 +14,15 @@ const fg = require('fast-glob');
 
 const specFileSuffix = '.spec.ts';
 
-export async function crawl(suiteDir: string): Promise<TestSuiteListingEntry[]> {
+export async function crawl(suite: string): Promise<TestSuiteListingEntry[]> {
+  const suiteDir = 'src/' + suite;
   if (!fs.existsSync(suiteDir)) {
     console.error(`Could not find ${suiteDir}`);
     process.exit(1);
   }
 
-  const glob = `**/{README.txt,*${specFileSuffix}}`;
-  const filesToEnumerate: string[] = await fg(glob, { onlyFiles: true, cwd: suiteDir });
+  const glob = `${suiteDir}/**/{README.txt,*${specFileSuffix}}`;
+  const filesToEnumerate: string[] = await fg(glob, { onlyFiles: true });
   filesToEnumerate.sort();
 
   const entries: TestSuiteListingEntry[] = [];
@@ -64,5 +65,6 @@ export async function crawl(suiteDir: string): Promise<TestSuiteListingEntry[]> 
 }
 
 export function makeListing(filename: string): Promise<TestSuiteListing> {
-  return crawl(path.dirname(filename));
+  const suite = path.basename(path.dirname(filename));
+  return crawl(suite);
 }
--- src/common/tools/gen_listings.ts
+++ src/common/tools/gen_listings.ts
@@ -1,33 +1,32 @@
 import * as fs from 'fs';
 import * as path from 'path';
+import * as process from 'process';
 
 import { crawl } from './crawl.js';
 
 function usage(rc: number): void {
   console.error('Usage:');
-  console.error('  tools/gen_listings [OUT_DIR] [SUITES...]');
-  console.error('  tools/gen_listings generated/ webgpu unittests');
+  console.error('  tools/gen_listings [SUITES...]');
+  console.error('  tools/gen_listings webgpu unittests');
   process.exit(rc);
 }
 
-if (process.argv.length <= 3) {
+if (process.argv.length <= 2) {
   usage(0);
 }
 
 const myself = 'src/common/tools/gen_listings.ts';
-// if (!fs.existsSync(myself)) {
-//   console.error('Must be run from repository root');
-//   usage(1);
-// }
-
-const outDir = process.argv[2];
+if (!fs.existsSync(myself)) {
+  console.error('Must be run from repository root');
+  usage(1);
+}
 
 (async () => {
-  for (const suite of process.argv.slice(3)) {
-    const listing = await crawl(path.join(path.dirname(path.dirname(__dirname)), suite));
+  for (const suite of process.argv.slice(2)) {
+    const listing = await crawl(suite);
 
-    const outFile = path.normalize(path.join(outDir, `${suite}/listing.js`));
-    fs.mkdirSync(path.join(outDir, suite), { recursive: true });
+    const outFile = path.normalize(`out/${suite}/listing.js`);
+    fs.mkdirSync(`out/${suite}`, { recursive: true });
     fs.writeFileSync(
       outFile,
       `\
